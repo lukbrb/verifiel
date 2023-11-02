03f7d9f2e1d10c2dc7a06e0d2936127205e569ff
lukbrb <lucasbarbr@outlook.com>
2023-11-02 19:13:31 +0100

    creation classe Message

--- verifiel/envoi.py
+++ verifiel/envoi.py
@@ -1,4 +1,5 @@
 import smtplib
+from dataclasses import dataclass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -10,26 +11,32 @@ PORT = 587
 text = "Vérifiel a envoyé ce courriel pour vous désabonner du message « list-unsubscribe »."
 
 
-def prepare_message(expediteur, destinataire, texte, sujet):
-    """
-    Prépare le contenu et les métadonnées du message à envoyer.
-    """
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = str(sujet)
-    msg['From'] = expediteur
-    msg['To'] = destinataire
-    part1 = MIMEText(texte, 'plain')
-    msg.attach(part1)
-    return msg
-
-
-def envoie_message(expediteur: str, destinataire: str, mot_de_passe: str, message: MIMEMultipart) -> None:
-    """
-        Envoie un courriel à l'adresse spécifiée par l'argument 'destinataire'
-    """
-
-    with smtplib.SMTP(SERVEUR, PORT) as server:
-        server.starttls()
-        server.login(expediteur, mot_de_passe)
-        server.sendmail(expediteur, destinataire, message.as_string())
-        print(f"[+] Message envoyé avec succès à {destinataire}!")
+class Message:
+    def __init__(self, expediteur: str, destinataire: str, texte: str, sujet: str) -> None:
+        self.exp = expediteur
+        self.dest = destinataire
+        self.txt = texte
+        self.sujet = sujet
+    
+    def _prepare_message(self) -> MIMEMultipart:
+        """
+        Prépare le contenu et les métadonnées du message à envoyer.
+        """
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = str(self.sujet)
+        msg['From'] = self.exp
+        msg['To'] = self.dest
+        part1 = MIMEText(self.txt, 'plain')
+        msg.attach(part1)
+        return msg
+    
+    def envoie_message(self, mot_de_passe: str) -> None:
+        """
+            Envoie un courriel à l'adresse spécifiée par l'argument 'destinataire'
+        """
+        message = self._prepare_message()
+        with smtplib.SMTP(SERVEUR, PORT) as server:
+            server.starttls()
+            server.login(self.exp, mot_de_passe)
+            server.sendmail(self.exp, self.dest, message.as_string())
+            print(f"[+] Message envoyé avec succès à {self.dest}!")
